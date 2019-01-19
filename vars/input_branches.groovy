50cedd147a5367804f3d65016e6ae9e8f37e07bd
Nicolas Arenas <nicolas.arenas@qindel.com>
2019-01-19 11:45:14 +0100

    Simple test

--- vars/input_branches.groovy
+++ vars/input_branches.groovy
@@ -1,8 +1,12 @@
 def input_box() {
-    def userInput = input(
-        id: 'userInput', message: 'Let\'s promote?', parameters: [
-        [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env'],
-        [$class: 'TextParameterDefinition', defaultValue: 'uat1', description: 'Target', name: 'target']
-    ])
-    return userInput
+    echo "Hola"
+}
+
+// def input_box() {
+//     def userInput = input(
+//         id: 'userInput', message: 'Let\'s promote?', parameters: [
+//         [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env'],
+//         [$class: 'TextParameterDefinition', defaultValue: 'uat1', description: 'Target', name: 'target']
+//     ])
+//     return userInput
 }
