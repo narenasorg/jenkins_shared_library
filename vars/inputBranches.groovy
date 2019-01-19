c7d7486b19c019a7fd982beb1b699a09f1c3271c
Nicolas Arenas <nicolas.arenas@qindel.com>
2019-01-19 12:21:34 +0100

    Test against call

--- vars/inputBranches.groovy
+++ vars/inputBranches.groovy
@@ -1,12 +1,7 @@
-def call∫() {
-    echo "Hola"
+def call() {
+    input(
+        id: 'userInput', message: 'Let\'s promote?', parameters: [
+        [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env'],
+        [$class: 'TextParameterDefinition', defaultValue: 'uat1', description: 'Target', name: 'target']
+    ])
 }
-
-// def input_box() {
-//     def userInput = input(
-//         id: 'userInput', message: 'Let\'s promote?', parameters: [
-//         [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env'],
-//         [$class: 'TextParameterDefinition', defaultValue: 'uat1', description: 'Target', name: 'target']
-//     ])
-//     return userInput
-//}
