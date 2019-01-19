111416acb1edc0368650ab75d98e5ba22cb05f2e
Nicolas Arenas <nicolas.arenas@qindel.com>
2019-01-19 11:28:01 +0100

    return value

--- vars/input_branches.groovy
+++ vars/input_branches.groovy
@@ -1,5 +1,9 @@
-def call(body) = input(
-    id: 'userInput', message: 'Let\'s promote?', parameters: [
-    [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env'],
-    [$class: 'TextParameterDefinition', defaultValue: 'uat1', description: 'Target', name: 'target']
+def input_box
+    {
+    def userInput = input(
+        id: 'userInput', message: 'Let\'s promote?', parameters: [
+        [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env'],
+        [$class: 'TextParameterDefinition', defaultValue: 'uat1', description: 'Target', name: 'target']
     ])
+    return userInput 
+}
