8537dec1532b211be5150a56997e6642bbddf990
Nicolas Arenas <nicolas.arenas@qindel.com>
2019-01-19 12:25:05 +0100

    input step to variable

--- vars/inputBranches.groovy
+++ vars/inputBranches.groovy
@@ -1,7 +1,8 @@
 def call() {
-    input(
+    def userInput = input(
         id: 'userInput', message: 'Let\'s promote?', parameters: [
         [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env'],
         [$class: 'TextParameterDefinition', defaultValue: 'uat1', description: 'Target', name: 'target']
     ])
+    return userInput
 }
