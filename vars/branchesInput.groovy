59f3b1b017612e87b097b6aa5eba66bebb294301
Nicolas Arenas <nicolas.arenas@qindel.com>
2019-01-21 07:06:18 +0100

    Dropdown provider from plugin

--- vars/branchesInput.groovy
+++ vars/branchesInput.groovy
@@ -2,10 +2,19 @@ def call() {
     data = [1,2,3,4]
     branch = input(
         id: 'userInput', message: 'Escoja una rama', parameters: [
-        [$class: 'ChoiceParameterDefinition',
-            choices: data ,
+        [$class: 'DropdownAutocompleteParameterDefinition',
             description: 'dropdown',
-            name: 'dropdown'
+            name: 'dropdown' ,
+            displayExpression: "name" ,
+            valueExpression: "name",
+            dataProvider: [
+                $class: "InlineJsonDataProvider",
+                autoCompleteData: [
+                    {"name": "Eddard Stark","id":"estark", "house":"Stark"},
+                    {"name": "John Snow","id":"jsnow", "house":"Stark"},
+                    {"name": "Tyrion Lannister","id":"tlannister", "house":"Lannister"},
+                    ]
+                ]
             ]
         ])
     return branch
